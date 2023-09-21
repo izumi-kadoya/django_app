69e2ddad464b101899ff7ed20f4d64412d9a0171
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-09-21 15:35:55 +0900

    2-34まで

--- hello/views.py
+++ hello/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import HelloForm
 
 def index(request):
   params = {
-    'title':'Hello/Index',
-    'msg':'これは、サンプルで作ったページです。',
-    'goto':'next',
+    'title': 'Hello',
+    'message': 'your data:',
+    'form': HelloForm()
   }
-  return render(request, 'hello/index.html', params)
-
-def next(request):
-  params = {
-    'title':'Hello/Next',
-    'msg':'これは、もう１つのページです。',
-    'goto':'index',
-  }
+  if (request.method == 'POST'):
+    params['message'] = '名前：' + request.POST['name'] + \
+      '<br>メール：' + request.POST['mail'] + \
+      '<br>年齢：' + request.POST['age']
+    params['form'] = HelloForm(request.POST)
   return render(request, 'hello/index.html', params)
